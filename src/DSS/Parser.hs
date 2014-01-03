51925bc05ce8774647bbf6b4eb29ed5e65b7836a
minamiyama1994 <masakazu.minamiyama@gmail.com>
2014-01-04 03:13:06 +0900

    use Either

--- src/DSS/Parser.hs
+++ src/DSS/Parser.hs
@@ -6,16 +6,15 @@ module DSS.Parser where
 
     data Discussion = Claim | Consent | Correction | Rebuttal | Supplement | Tree deriving ( Show )
     data Statement = TopStatement String | IndentStatement [ String ] deriving ( Show )
-    data PreStatement = PreTop String | PreIndent String
 
     parse :: String -> [ Statement ]
     parse str = map toStatement $ groupBy isSame $ map lineParse $ filter ( not . null ) $ lines str where
-        isSame ( PreIndent _ ) ( PreIndent _ ) = True
+        isSame ( Right _ ) ( Right _ ) = True
         isSame _ _ = False
-        toStatement [ PreTop s ] = TopStatement s
-        toStatement xs = IndentStatement $ map ( \ ( PreIndent x ) -> x ) xs
+        toStatement [ Left s ] = TopStatement s
+        toStatement xs = IndentStatement $ map ( \ ( Right x ) -> x ) xs
 
-    lineParse :: String -> PreStatement
+    lineParse :: String -> Either String String
     lineParse str = case str =~ "^\\s*" :: ( String , String , String ) of
-        ( _ , "" , body ) -> PreTop body
-        ( _ , _ , body ) -> PreIndent body
+        ( _ , "" , body ) -> Left body
+        ( _ , _ , body ) -> Right body
