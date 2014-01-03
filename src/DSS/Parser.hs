970121c31d131f4205a5e9e70bfbe2d25c2f23f9
minamiyama1994 <masakazu.minamiyama@gmail.com>
2014-01-04 02:17:09 +0900

    writing code in TDD manner.

--- src/DSS/Parser.hs
+++ src/DSS/Parser.hs
@@ -1,9 +1,21 @@
 module DSS.Parser where
-    data Category = ClaimCategory Claim | ConsentCategory Consent | CorrectionCategory Correction | RebuttalCategory Rebuttal | SupplementCategory Supplement | Tree deriving ( Show )
-    data Claim = Claim deriving ( Show )
-    data Consent = Consent deriving ( Show )
-    data Correction = Correction deriving ( Show )
-    data Rebuttal = Rebuttal deriving ( Show )
-    data Supplement = Supplement deriving ( Show )
-    parse :: String -> Category
-    parse _ = Tree
+
+    import Text.Regex
+    import Text.Regex.Posix
+    import Data.List
+
+    data Discussion = Claim | Consent | Correction | Rebuttal | Supplement | Tree deriving ( Show )
+    data Statement = TopStatement String | IndentStatement [ String ] deriving ( Show )
+    data PreStatement = PreTop String | PreIndent String
+
+    parse :: String -> [ Statement ]
+    parse str = map toStatement $ groupBy isSame $ map lineParse $ filter ( not . null ) $ lines str where
+        isSame ( PreIndent _ ) ( PreIndent _ ) = True
+        isSame _ _ = False
+        toStatement [ PreTop s ] = TopStatement s
+        toStatement xs = IndentStatement $ map ( \ ( PreIndent x ) -> x ) xs
+
+    lineParse :: String -> PreStatement
+    lineParse str = case str =~ "^\\s*" :: ( String , String , String ) of
+        ( _ , "" , body ) -> PreTop body
+        ( _ , _ , body ) -> PreIndent body
